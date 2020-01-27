71d42b8ae70db946fe427dda706e691e3da2a756
oscardenas17 <oscardenas17@gmail.com>
2020-01-26 19:25:24 -0500

    configurando links, vistas , un solo llamado head

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,11 @@ const router = express.Router();
 
 const pool = require('../database');
 
+//Views (Template Engine):
+router.get('/add',(req,res)=>{
+    res.render('links/add');
+    }
+)
+
+
 module.exports = router;
